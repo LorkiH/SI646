bcc061b36bdd6011cabdaabd73f37d459fe4fdc7
Lora Dimitrova <loraDimitrova203@gmail.com>
2025-04-29 13:24:44 +0300

    Упражнение №1

---
+++ prime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool is_prime(int num){
+    bool prime = true;
+    int end=(int)sqrt((double)num);
+    for(int i=2; i<=end;i++)
+    {
+        if(num % i == 0)
+        {
+            prime=false;
+        }
+    }
+}
+int main()
+{
+    int num;
+    scanf("%d",&num); 
+    
+    if(is_prime(num)){
+        printf("the number is prime");
+    }
+    else{
+        printf("the number is not prime");
+    }
+    return EXIT_SUCCESS;
+}
