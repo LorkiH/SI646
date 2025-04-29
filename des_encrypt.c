6eac3f279191e4f811f50fc91d421e934658fbaf
Lora Dimitrova <loraDimitrova203@gmail.com>
2025-04-29 13:34:59 +0300

    Упражнение №7

---
+++ des_encrypt.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/des.h>
+#include <stdlib.h>
+#define KEY_SIZE 8
+#define MAXN 100
+
+int main()
+{
+    unsigned char key[KEY_SIZE] = "banichka";
+    unsigned char input[MAXN];
+    fgets((char*)input,MAXN,stdin);
+    input[strcspn(input,"\n")]='\0';
+    unsigned int length = strlen(input);
+    int padded_lenght = ((length + 7) /8)*8;
+    unsigned char padded_input[padded_lenght];
+    unsigned char output[padded_lenght];
+
+    memcpy(padded_input,input,length);
+    memset(padded_input+length,padded_lenght-length,padded_lenght-length);
+
+    DES_key_schedule key_schedule;
+    DES_set_key( (DES_cblock*) key, &key_schedule);
+   
+    for(int i = 0; i < padded_lenght; i+=8)
+    {
+        DES_ecb_encrypt((DES_cblock*) (padded_input + i), (DES_cblock*) (output + i), &key_schedule, DES_ENCRYPT);
+    }
+
+    for(int i = 0; i < padded_lenght; i++)
+    {
+        printf("%02x", output[i]);
+    }
+
+    return EXIT_SUCCESS;
+}
+    
